94ba48fd096d72b07f14d3ad4498d092abae3e1a
Gabriel Mendes da Silva <122908813+GabrielMendes06@users.noreply.github.com>
2024-04-25 14:32:54 -0300

    feat: Create header, add new favicon and change page title & description

--- __test__/page.test.jsx
+++ __test__/page.test.jsx
@@ -1,10 +1,8 @@
-import '@testing-library/jest-dom'
- 
 describe('Page', () => {
   test('renders a heading', () => {
     const sum = (a, b) => {
       return a + b
     }
-    expect(3).toBe(sum(1, 2))
-  })
+    expect(sum(1, 2)).toBe(3)
+  }) 
 })
