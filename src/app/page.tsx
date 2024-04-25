df11cfe46d6fc43d77badc4ae729ff181215c76a
Gabriel Mendes da Silva <122908813+GabrielMendes06@users.noreply.github.com>
2024-04-24 21:35:21 -0300

    feat: Add framer-motion in the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+'use client'
+
 import variables from './variables.module.scss'
+import { motion } from 'framer-motion'
 
 export default function Home() {
   return (
-    <h1 style={{ color: variables.primaryColor }}>Hello, Next.js!</h1>
+    <div>
+      <h1 style={{ color: variables.primaryColor }}>Hello, Next.js!</h1>
+      <motion.div
+      className='w-52 h-52 bg-black'
+        whileHover={{ scale: 1.2 }}
+        whileTap={{ scale: 1.1 }}
+        drag='x'
+        dragConstraints={{ left: -100, right: 100 }}
+      ></motion.div>
+    </div>
   )
 }
