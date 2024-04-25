3e6ad242fde348942f958ab8a42320ec9d31e2d2
Gabriel Mendes da Silva <122908813+GabrielMendes06@users.noreply.github.com>
2024-04-25 10:35:33 -0300

    feat: Add jest types to autocomplete functions during development

--- __tests__/page.test.jsx
+++ __tests__/page.test.jsx
@@ -1,13 +1,19 @@
-import '@testing-library/jest-dom'
+
 import { render, screen } from '@testing-library/react'
 import Page from '../src/app/page'
  
 describe('Page', () => {
-  it('renders a heading', () => {
+  test('renders a heading', () => {
     render(<Page />)
  
     const heading = screen.getByRole('heading', { level: 1 })
  
     expect(heading).toBeInTheDocument()
+  })
+
+  test('Get text', () => {
+    const { getByText } = render(<Page />)
+
+    expect(getByText('Hello, Next.js!')).toBeInTheDocument()
   })
 })
